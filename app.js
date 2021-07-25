6381cd34046088c3b242d8ec2ffe8d26baaa2a1b
ramalingaa <87629304+ramalingaa@users.noreply.github.com>
2021-07-25 12:08:50 +0530

    style:Adding styles to the page

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 var btnClick = document.querySelector("#btn-click");
 var txtClick = document.querySelector("#txt-area");
 var txtOutput = document.querySelector("#txt-click");
+var url = "https://api.funtranslations.com/translate/yoda.json";
+//requesting API server with input text 
+    function inputHandler(inputTxt){
+        return url + "?" +"text=" + inputTxt;
+    }
+    //Error handling message
+    function errorHandler(error){
+        console.log("Server can only handle 5 requests per hour, please try after some time");
+        alert("Server can only handle 5 requests per hour, please try after One hour")
+    }
+    function clickHandler(){
+            var textInput = txtClick.value;// inputvalue
+            // fetcing the server
+        fetch(inputHandler(textInput))
+        .then(response => response.json())
+        .then(json=>{
+            
+            var outputText = json.contents.translated;//convert output from json
+            txtOutput.innerText = outputText;// display output
 
- btnClick.addEventListener("click",function clickHandler()
-    {
-        txtOutput.innerText = txtClick.value;
-}
- )
+        })
+        .catch(errorHandler)
+    };
+    btnClick.addEventListener("click",clickHandler)
